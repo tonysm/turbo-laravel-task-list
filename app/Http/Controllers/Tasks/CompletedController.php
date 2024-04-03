b544e59c1a743530542d71b68f08f2a59994b511
Tony Messias <tonysm@hey.com>
2024-04-02 23:49:43 -0300

    No need to refresh on mobule after task completion, just redirect back

--- app/Http/Controllers/Tasks/CompletedController.php
+++ app/Http/Controllers/Tasks/CompletedController.php
@@ -4,16 +4,13 @@ namespace App\Http\Controllers\Tasks;
 
 use App\Http\Controllers\Controller;
 use App\Models\Task;
-use HotwiredLaravel\TurboLaravel\Http\Controllers\Concerns\InteractsWithTurboNativeNavigation;
 
 class CompletedController extends Controller
 {
-    use InteractsWithTurboNativeNavigation;
-
     public function update(Task $task)
     {
         $task->toggleCompletion();
 
-        return $this->refreshOrRedirectBack(route('dashboard'))->with('notice', __('Task Updated.'));
+        return back()->with('notice', __('Task Updated.'));
     }
 }
